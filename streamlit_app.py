28507ba2c6468f2dcc2ffb9383828b2f86389e62
andrewjohnson10 <147972486+andrewjohnson10@users.noreply.github.com>
2023-10-15 17:01:11 +0800

    Update streamlit_app.py
    Added avocado toast to breakfast menu.
    Added emoji icons to breakfast menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,9 @@ streamlit.title('My parents healthy diner')
 
 # add breakfast menu to application
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 Oatmeal')
-streamlit.text('Kale and rocket smoothie')
-streamlit.text('Hard-boiled egg')
 
+# breakfast menu items
+streamlit.text('🥣 Omega 3 Oatmeal')
+streamlit.text('🥗 Kale and rocket smoothie')
+streamlit.text('🐔 Hard-boiled egg')
+streamlit.text('🥑🍞 Avocardo toast')
